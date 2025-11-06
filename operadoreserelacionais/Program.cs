f345f9776f1beb2aa3be828fc0af4e95be6bc5c5
VICTOR PAULO CARVALHO RIBEIRO <victor.pcribeiro@senacsp.edu.br>
2025-11-06 15:02:40 -0300

    atualização

---
+++ operadoreserelacionais/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = 10;
+int b = 5;
+
+//operadores relacionais
+bool maiorQue = a > b;
+bool menorQue = a < b;
+bool igual = a == b;
+bool diferente = a != b;
+bool maiorOuIgual = a >= b;
+bool menorOuigual = a <= b;
+
+//Exibindo resultados nos operadores relacionais
+Console.WriteLine("\nOperadores Relacionais:");
+Console.WriteLine($"{a} > {b}: {maiorQue}");
+Console.WriteLine($"{a} < {b}: {menorQue}");
+Console.WriteLine($"{a} == {b}: {igual}");
+Console.WriteLine($"{a} != {b}: {diferente}");
+Console.WriteLine($"{a} >= {b}: {maiorOuIgual}");
+Console.WriteLine($"{a} <= {b}: {menorOuigual}");
---
+++ operadoreslogicos/Program.cs
@@ -0,0 +1,13 @@
+﻿//operadores logicos
+bool x = true;
+bool y = false;
+
+bool and = x && y; //E Lógico
+bool or = x || y;  // Ou Lógico
+bool not = !x;    // Não lógico
+
+// Exibindo resultados dos operadores lógicos
+Console.WriteLine("\nOperadores Lógicos:");
+Console.WriteLine($"x && y:{and}"); //E lógico
+Console.WriteLine($"x || y:{or}"); //Ou Lógico
+Console.WriteLine($"!x :{not}"); //Não lógico
